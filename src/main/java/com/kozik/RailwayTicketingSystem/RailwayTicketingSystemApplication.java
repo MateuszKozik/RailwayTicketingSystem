900fc4af667aab3abc14aae7a69077732761797b
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-22 12:45:18 +0100

    Init new repository

---
+++ src/main/java/com/kozik/RailwayTicketingSystem/RailwayTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kozik.RailwayTicketingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kozik/RailwayTicketingSystem/RailwayTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozik.RailwayTicketingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
