46ba9750b33dddbcac3faa8dbe2fb769d532c50f
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-23 12:44:50 +0100

    Changed uzytkownik entity

--- src/main/java/com/kozik/RailwayTicketingSystem/entities/Uzytkownik.java
+++ src/main/java/com/kozik/RailwayTicketingSystem/entities/Uzytkownik.java
@@ -2,8 +2,6 @@ package com.kozik.RailwayTicketingSystem.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -12,23 +10,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "uzytkownik")
 public class Uzytkownik {
-
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id_uzytkownika", nullable = false)
-    private Long idUzytkownika;
-
-    @Column(name = "nazwa_uzytkownika", nullable = false, unique = true)
-    private String nazwaUzytkownika;
-
-    @Column(name = "haslo", nullable = false)
-    private String haslo;
-
     @Column(name = "email", nullable = false)
     private String email;
-
-    @Column(name = "typ_konta", nullable = false)
-    private String typKonta = "USER";
+    
+    @Column(name = "haslo", nullable = false)
+    private String haslo;
 
     @Column(name = "enabled", nullable = false)
     private Boolean enabled = true;
@@ -42,36 +30,11 @@ public class Uzytkownik {
     public Uzytkownik() {
     }
 
-    public Uzytkownik(String nazwaUzytkownika, String haslo, String email) {
-        this.nazwaUzytkownika = nazwaUzytkownika;
+    public Uzytkownik( String haslo, String email) {
         this.haslo = haslo;
         this.email = email;
         maszynista = null;
         enabled = true;
-    }
-
-    public Long getIdUzytkownika() {
-        return idUzytkownika;
-    }
-
-    public void setIdUzytkownika(Long idUzytkownika) {
-        this.idUzytkownika = idUzytkownika;
-    }
-
-    public String getNazwaUzytkownika() {
-        return nazwaUzytkownika;
-    }
-
-    public void setNazwaUzytkownika(String nazwaUzytkownika) {
-        this.nazwaUzytkownika = nazwaUzytkownika;
-    }
-
-    public String getHaslo() {
-        return haslo;
-    }
-
-    public void setHaslo(String haslo) {
-        this.haslo = haslo;
     }
 
     public String getEmail() {
@@ -82,20 +45,12 @@ public class Uzytkownik {
         this.email = email;
     }
 
-    public String getTypKonta() {
-        return typKonta;
+    public String getHaslo() {
+        return haslo;
     }
 
-    public void setTypKonta(String typKonta) {
-        this.typKonta = typKonta;
-    }
-
-    public Maszynista getMaszynista() {
-        return maszynista;
-    }
-
-    public void setMaszynista(Maszynista maszynista) {
-        this.maszynista = maszynista;
+    public void setHaslo(String haslo) {
+        this.haslo = haslo;
     }
 
     public Boolean getEnabled() {
@@ -106,17 +61,20 @@ public class Uzytkownik {
         this.enabled = enabled;
     }
 
+    public Maszynista getMaszynista() {
+        return maszynista;
+    }
+
+    public void setMaszynista(Maszynista maszynista) {
+        this.maszynista = maszynista;
+    }
+
     public Pasazer getPasazer() {
         return pasazer;
     }
 
     public void setPasazer(Pasazer pasazer) {
         this.pasazer = pasazer;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("[%s - %s - %s - %s]", idUzytkownika, nazwaUzytkownika, haslo, typKonta);
-    }
+    }  
 }
 
