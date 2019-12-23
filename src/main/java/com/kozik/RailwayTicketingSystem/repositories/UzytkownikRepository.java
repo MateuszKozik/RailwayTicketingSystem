35e0526775ef67619088a6c07cae9b278a7e54af
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-23 13:05:30 +0100

    Changed uzytkownik repository to String id

--- src/main/java/com/kozik/RailwayTicketingSystem/repositories/UzytkownikRepository.java
+++ src/main/java/com/kozik/RailwayTicketingSystem/repositories/UzytkownikRepository.java
@@ -3,6 +3,6 @@ package com.kozik.RailwayTicketingSystem.repositories;
 import com.kozik.RailwayTicketingSystem.entities.Uzytkownik;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UzytkownikRepository extends JpaRepository<Uzytkownik, Long>{
+public interface UzytkownikRepository extends JpaRepository<Uzytkownik, String>{
     
 }
