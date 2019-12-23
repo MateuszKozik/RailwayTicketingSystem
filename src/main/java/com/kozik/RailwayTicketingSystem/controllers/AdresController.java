fd3a30acf612663ad1478e97ad5cf38d607920e6
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-23 10:36:59 +0100

    Added controller and view to edit adres

--- src/main/java/com/kozik/RailwayTicketingSystem/controllers/AdresController.java
+++ src/main/java/com/kozik/RailwayTicketingSystem/controllers/AdresController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdresController {
@@ -18,8 +20,6 @@ public class AdresController {
     @RequestMapping(value = "views/adres/list", method = RequestMethod.GET)
     public String getAll(Model model){
         List<Adres> adresList = adresService.listAll();
-                          
-        model.addAttribute("header","Lista wszystkich adresów"); 
         model.addAttribute("adresList",adresList);
         return "/views/adres/list";
     }
@@ -36,4 +36,21 @@ public class AdresController {
         adresService.save(adres);
         return "redirect:/views/adres/list";
     }
+    
+     @RequestMapping(value = "views/adres/edit/{id}", method = RequestMethod.GET)
+    public String edit(Model model,@PathVariable(name = "id") long id){
+        
+        Adres adres = adresService.get(id);
+        model.addAttribute("adres", adres);
+        return "/views/adres/edit";
+    }
+    
+    @RequestMapping(value = "views/adres/edit/{id}", method = RequestMethod.POST)
+    public String edit(@PathVariable(name = "id") long id, @ModelAttribute("adres") Adres adres){ 
+        System.out.println(id);
+        adres.setIdAdresu(id);
+        adresService.save(adres);
+        return "redirect:/views/adres/list";
+    }
+    
 }
