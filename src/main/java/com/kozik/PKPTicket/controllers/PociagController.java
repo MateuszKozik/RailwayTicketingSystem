623c1b050d4cb9c59d24c5b011928aa4f9281073
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-26 16:53:23 +0100

    You can set maszynista in the pociag view

--- src/main/java/com/kozik/PKPTicket/controllers/PociagController.java
+++ src/main/java/com/kozik/PKPTicket/controllers/PociagController.java
@@ -1,6 +1,8 @@
 package com.kozik.PKPTicket.controllers;
 
 import com.kozik.PKPTicket.entities.Pociag;
+import com.kozik.PKPTicket.entities.Maszynista;
+import com.kozik.PKPTicket.services.MaszynistaService;
 import com.kozik.PKPTicket.services.PociagService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PociagController {
     
-    @Autowired
-    private PociagService pociagService;
+    @Autowired private PociagService pociagService;
+    @Autowired private MaszynistaService maszynistaService;
     
     @RequestMapping(value = "/pociag/list", method = RequestMethod.GET)
     public String getAll(Model model){
@@ -26,28 +29,36 @@ public class PociagController {
     
     @RequestMapping(value = "/pociag/add", method = RequestMethod.GET)
     public String add(Model model){
+        List<Maszynista> maszynistaList = maszynistaService.listAll();
         Pociag pociag = new Pociag();
         model.addAttribute("pociag", pociag);
+        model.addAttribute("maszynistaList", maszynistaList);
         return "views/pociag/add";
     }
     
     @RequestMapping(value = "/pociag/add", method = RequestMethod.POST)
-    public String add(@ModelAttribute("pociag") Pociag pociag){
+    public String add(@ModelAttribute("pociag") Pociag pociag,
+            @RequestParam("maszynista") Maszynista maszynista){
+        pociag.setMaszynista(maszynista);
         pociagService.save(pociag);
         return "redirect:/pociag/list";
     }
     
     @RequestMapping(value = "/pociag/edit/{id}", method = RequestMethod.GET)
     public String edit(Model model, @PathVariable(name = "id") long id){
+        List<Maszynista> maszynistaList = maszynistaService.listAll();
         Pociag pociag = pociagService.get(id);
         model.addAttribute("pociag", pociag);
+        model.addAttribute("maszynistaList", maszynistaList);
         return "views/pociag/edit";
     }
     
     @RequestMapping(value = "/pociag/edit/{id}", method = RequestMethod.POST)
     public String edit(@PathVariable(name = "id") long id,
-            @ModelAttribute("pociag") Pociag pociag){
+            @ModelAttribute("pociag") Pociag pociag,
+            @RequestParam("maszynista") Maszynista maszynista){
         pociag.setIdPociagu(id);
+        pociag.setMaszynista(maszynista);
         pociagService.save(pociag);
         return "redirect:/pociag/list";
     }
