9ce38c12630e92a1ab072a8463dd2809fb075fc8
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-27 12:59:57 +0100

    Prepared security config

---
+++ src/main/java/com/kozik/PKPTicket/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.kozik.PKPTicket.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select email as principal, password as credentials, enabled from uzytkownik where email=?")
+                .authoritiesByUsernameQuery("select uzytkownik_email as principal, uprawnienia_nazwa as role from uzytkownik_uprawnienia where uzytkownik_email = ? ");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/regster", "/", "/home", "/login", "/css/**").permitAll()
+                .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+                .defaultSuccessUrl("/profile").and().logout().logoutSuccessUrl("/login");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
