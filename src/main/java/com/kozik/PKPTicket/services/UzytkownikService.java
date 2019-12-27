94f77ed1110a568e8e4909c7b10a36184a167024
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-27 13:06:15 +0100

    Passwords will be encrypted during registration

--- src/main/java/com/kozik/PKPTicket/services/UzytkownikService.java
+++ src/main/java/com/kozik/PKPTicket/services/UzytkownikService.java
@@ -5,6 +5,7 @@ import com.kozik.PKPTicket.entities.Uzytkownik;
 import com.kozik.PKPTicket.repositories.UzytkownikRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,7 +18,10 @@ public class UzytkownikService {
     }
     
     public void save(Uzytkownik uzytkownik, List<Uprawnienia> uprawnienia){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        uzytkownik.setHaslo(encoder.encode(uzytkownik.getHaslo()));
         uzytkownik.setUprawnienia(uprawnienia);
+        
         uzytkownikRepository.save(uzytkownik);
     }
     
