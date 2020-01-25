4655bb9228ac61ec3ec4d34e166af072a54ce3b9
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-01-25 13:28:34 +0100

    Added znizka fields validation

--- src/main/java/com/kozik/PKPTicket/controllers/ZnizkaController.java
+++ src/main/java/com/kozik/PKPTicket/controllers/ZnizkaController.java
@@ -3,9 +3,13 @@ package com.kozik.PKPTicket.controllers;
 import com.kozik.PKPTicket.entities.Znizka;
 import com.kozik.PKPTicket.services.ZnizkaService;
 import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +36,10 @@ public class ZnizkaController {
     }
     
     @RequestMapping(value = "znizka/add", method = RequestMethod.POST)
-    public String save(@ModelAttribute("znizka") Znizka znizka){
+    public String save(@Valid Znizka znizka, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "views/znizka/add";
+        }
         znizkaService.save(znizka);
         return "redirect:/znizka/list";
     }
@@ -45,7 +52,11 @@ public class ZnizkaController {
     }
     
     @RequestMapping(value = "/znizka/edit/{id}", method = RequestMethod.POST)
-    public String edit(@PathVariable(name = "id") long id, @ModelAttribute("pasazer") Znizka znizka){ 
+    public String edit(@PathVariable(name = "id") long id, 
+    @Valid Znizka znizka, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "views/znizka/edit";
+        } 
         znizka.setIdZnizki(id);
         znizkaService.save(znizka);
         return "redirect:/znizka/list";
--- src/main/java/com/kozik/PKPTicket/entities/Znizka.java
+++ src/main/java/com/kozik/PKPTicket/entities/Znizka.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "znizka")
@@ -19,6 +20,7 @@ public class Znizka {
     @Column(name = "id_znizki", nullable = false)
     private Long idZnizki;
     
+    @NotEmpty
     @Column(name = "opis", nullable = false)
     private String opis;
     
